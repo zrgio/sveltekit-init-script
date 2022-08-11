702fcf73bcf4e9b662f8ff9f23606ff10deaf602
Sergio <sergioulloa@protonmail.com>
2022-08-11 01:34:42 -0400

    oops

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,10 +6,10 @@ npm install -D --save-dev tailwindcss@latest postcss@latest autoprefixer@latest 
 
 npx tailwindcss init tailwind.config.cjs -p
 
-cp patch_files/app.css $project/src/
+cp patch_files/app.css src/
 
-cp patch_files/__layout.svelte $project/src/routes/
+cp patch_files/__layout.svelte src/routes/
 
-patch $project/package.json patch_files/package.json.patch
-patch $project/svelte.config.js patch_files/svelte.config.js.patch
-patch $project/tailwind.config.cjs patch_files/tailwind.config.cjs.patch
+patch package.json patch_files/package.json.patch
+patch svelte.config.js patch_files/svelte.config.js.patch
+patch tailwind.config.cjs patch_files/tailwind.config.cjs.patch
